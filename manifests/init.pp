3a59cbe8462db947bd7e77d1b8244a05c1bc9c36
Tobias Schneider <schneider@synyx.de>
2015-10-08 18:10:01 +0200

    add wallpaper

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,4 +9,12 @@ class client (){
     ensure          => installed,
     install_options => ['--allow-unauthenticated', '-f'],
   }
+
+  file { "/usr/share/backgrounds/devoxx4kids_wallpaper.png":
+    ensure  => present,
+    source  => "puppet:///modules/${module_name}/devoxx4kids_wallpaper.png",
+  } ->
+  exec { "set wallpaper":
+    command => "/usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/devoxx4kids_wallpaper.png",
+  }
 }
