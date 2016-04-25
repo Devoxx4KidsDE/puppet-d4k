bf6c26d38ccf3fb344c453a1bd8b7ceb3e983e8e
Tobias Schneider <schneider@synyx.de>
2016-04-25 14:03:19 +0200

    rename minecraft ws

--- manifests/workshop_minecraft_raspberry.pp
+++ manifests/workshop_minecraft_raspberry.pp
@@ -1,7 +1,7 @@
 class d4k::workshop_minecraft_collect_game {
 
   ensure_resource('package', ['minecraft-pi', 'python'],
-    { 'ensure' => 'present',
+    { 'ensure'          => 'present',
       'install_options' => ['--allow-unauthenticated', '-f']
     },
   )
@@ -10,6 +10,14 @@ class d4k::workshop_minecraft_collect_game {
     ensure   => latest,
     provider => git,
     source   => 'git@192.168.1.100:workshop-minecraft-modding-raspberry-pi',
+    revision => 'master',
+    user     => 'pi',
+  }
+
+  vcsrepo { '/home/pi/workshops/workshop-minecraft-hide-and-seek':
+    ensure   => latest,
+    provider => git,
+    source   => 'git@192.168.1.100:workshop-minecraft-hide-and-seek',
     revision => 'master',
     user     => 'pi',
   }
